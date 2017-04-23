7c12a931d7e32e0f6b43bc01580aabb0a5144ff3
Diogo Korok <korokdiogo@gmail.com>
2017-04-22 21:44:37 -0300

    feat: create a script to make http requests to pokeapi.co/

---
+++ reqTest.js
@@ -0,0 +1,29 @@
+const http = require('http');
+//const $ = require('jQuery');
+
+const movePath = '/api/v2/move/';
+const pokePath = '/api/v2/pokemon/';
+
+let options = {
+    host: 'pokeapi.co',
+    path: movePath + '1/',
+    headers: {
+        accept: 'application/json'
+    }
+};
+
+let result;
+
+http.get(options, (response) => {
+    response.setEncoding('utf8');
+    response.on('data', (chunk) => {
+        result = JSON.parse(chunk);
+        console.log(result);
+    });
+}).on("error", (e) => {
+    console.log("Got error: " + e.message);
+});
+
+/*$.getJSON(movePath + '1/', (data) => {
+    console.log(data);
+});*/
